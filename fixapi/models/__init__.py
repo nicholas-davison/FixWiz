20a0d84bbbce9fe08dab39557f86fdd494b2ce4f
Nicholas Davison <nick.a.davison@gmail.com>
2024-07-17 15:57:01 -0500

    created models, fixtures

---
+++ fixapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .customer import Customer
+from .contractor import Contractor
+from .service_request import ServiceRequest
+from .category import Category
+from .servicerequest_category import ServiceRequestCategory
