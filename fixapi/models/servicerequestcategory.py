ce32c0f3ecbeb7359a5633c38c80cd88e34ba01f
Nicholas Davison <nick.a.davison@gmail.com>
2024-07-18 13:47:01 -0500

    added related name to fields in servicerequestcategory model

--- fixapi/models/servicerequestcategory.py
+++ fixapi/models/servicerequestcategory.py
@@ -3,5 +3,5 @@ from .servicerequest import ServiceRequest
 from .category import Category
 
 class ServiceRequestCategory(models.Model):
-    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE, related_name="categories")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="service_requests")
