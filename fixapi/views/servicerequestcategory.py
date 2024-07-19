e9e8312ecf9ad1620746c76c5554a6fda1ccd6d9
Nicholas Davison <nick.a.davison@gmail.com>
2024-07-19 13:55:44 -0500

    CRUD for service requests categories

--- fixapi/views/servicerequestcategory.py
+++ fixapi/views/servicerequestcategory.py
@@ -8,7 +8,6 @@ from fixapi.models import ServiceRequestCategory
 class ServiceRequestCategoryView(ViewSet):
     """Void view set"""
 
-
     def create(self, request):
         """Handle POST operations
 
@@ -17,46 +16,14 @@ class ServiceRequestCategoryView(ViewSet):
         """
         service_request_category = ServiceRequestCategory()
         service_request_category.service_request = request.data["service_request"]
-        service_request_category = request.data["category"]
+        service_request_category.category = request.data["category"]
 
         try:
             service_request_category.save()
-            serializer = VoidSerializer(void)
+            serializer = ServiceRequestCategorySerializer(service_request_category)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as ex:
             return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        """Handle GET requests for single item
-
-        Returns:
-            Response -- JSON serialized instance
-        """
-        try:
-            void = Void.objects.get(pk=pk)
-            serializer = VoidSerializer(void)
-            return Response(serializer.data)
-        except Exception as ex:
-            return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        """Handle PUT requests
-
-        Returns:
-            Response -- Empty body with 204 status code
-        """
-        try:
-            void = Void.objects.get(pk=pk)
-            void.sample_name = request.data["name"]
-            void.sample_description = request.data["description"]
-            void.save()
-        except Void.DoesNotExist:
-            return Response(None, status=status.HTTP_404_NOT_FOUND)
-
-        except Exception as ex:
-            return HttpResponseServerError(ex)
-
-        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
         """Handle DELETE requests for a single item
@@ -65,11 +32,11 @@ class ServiceRequestCategoryView(ViewSet):
             Response -- 200, 404, or 500 status code
         """
         try:
-            void = Void.objects.get(pk=pk)
-            void.delete()
+            service_request_category = ServiceRequestCategory.objects.get(pk=pk)
+            service_request_category.delete()
             return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-        except Void.DoesNotExist as ex:
+        except service_request_category.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
@@ -82,8 +49,8 @@ class ServiceRequestCategoryView(ViewSet):
             Response -- JSON serialized array
         """
         try:
-            categories = Category.objects.all()
-            serializer = CategorySerializer(categories, many=True)
+            service_request_category = ServiceRequestCategory.objects.filter(service_request=request.data['service_request'])
+            serializer = ServiceRequestCategorySerializer(service_request_category, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
