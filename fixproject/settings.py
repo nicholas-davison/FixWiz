617c255fcf2a3e956f352e5d5fa053c5397f2f24
Nicholas Davison <nick.a.davison@gmail.com>
2024-09-20 17:01:21 -0500

    edited CORS whitelist URL

--- fixproject/settings.py
+++ fixproject/settings.py
@@ -50,7 +50,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://dolphin-app-nfouu.ondigitalocean.app/login'
+    'https://dolphin-app-nfouu.ondigitalocean.app'
 )
 
 
